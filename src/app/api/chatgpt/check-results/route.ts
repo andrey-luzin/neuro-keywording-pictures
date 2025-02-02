2cc6e79e42c3f0adca7a1fe927e08a2add97a3b1
Andrey Luzin <andrey.luzin@gmail.com>
2025-02-02 17:59:19 +0100

    replace promts to env

--- src/app/api/chatgpt/check-results/route.ts
+++ src/app/api/chatgpt/check-results/route.ts
@@ -15,6 +15,12 @@ export async function POST(req: Request): Promise<Response> {
     console.log('check-results data', data);
     const { fileName, keywords, description, model } = data;
 
+    const textTemplate = process.env.CHECK_RESULTS_PROMT || "";
+
+    const promt = textTemplate
+      .replace("${keywords}", keywords)
+      .replace("${description}", description);
+
     const response = await openai.chat.completions.create({
       model,
       messages: [
@@ -32,7 +38,7 @@ export async function POST(req: Request): Promise<Response> {
           content: JSON.stringify([
             {
               type: "text",
-              text: `From now on, pretend you are an art and photo expert with limitless knowledge about the microstock industry and search engines, highly capable of distilling specific words to describe photos and illustrations so they will be easy to find through search. I need to check and improve the keywords and description for stock images. I need your help. There are a list of keywords (${keywords}) and description (${description}). Your tasks are: - Check that the description complies with the following rules: - Be unique. - Be good for SEO. - Use 2-4 main keywords. - Be no more than 200 characters long. - Consist of 2 short sentences. - Have a detailed, descriptive tone similar If the description does not comply with the rules, then rewrite it. - Check for duplicate words in the list. Don’t indicate words with same base (like plant-based and plant), check only for exact duplicates. You can add words and move more important words to the top of the list that reflect the concept of important concepts or popular search queries. - Analyse topic and theme of content based on keywords and sort keywords by relevance (not alphabetically!). - Provide the list in a single line, separating words with commas. Provide only the corrected description and keywords in response`,
+              text: promt,
             },
           ]),
         },
